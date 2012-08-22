fda0fc3ed68d85fc04b42b3ef37eb7d7f39be5e9
aq <aku.peltonen@gmail.com>
2012-08-23 01:34:25 +0300

    Initial commit with bencode added to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bttracker::Application.config.secret_token = 'a39b2927f82bc43ba7c794052f7d9179f8c7ade321cbf27fb1a6db3c5f5cbeca0bfd945d46d11f1677a755fe2d937c25ce455e39a8d8ed296a486a77cb3c7385'
