ab28ed52527a53d964bf8727ee8ea189b4ee7e02
AQ <aku.peltonen@gmail.com>
2012-12-26 16:12:02 +0200

    Added a search field

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,10 +2,11 @@ jQuery(document).ready(function() {
     $(".alert").alert();
 
     $("input[id=file]").change(function() {
-        $("#file_path").val($(this).val());
+        $("#file_path").val($(this)[0].files[0].name);
     });
 
-    $("#fake_file_button").on("click", function() {
+    $("#fake_file_button").on("click", function(e) {
+		e.preventDefault();
         $('input[id=file]').click();
     });
 });
